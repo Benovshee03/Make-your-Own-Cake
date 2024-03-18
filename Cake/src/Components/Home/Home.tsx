6c7b74ac945face738cb4647c326c37573a495ea
Benovshe <benovshee03@gmail.com>
2024-03-18 23:48:01 +0400

    home changed

--- Cake/src/Components/Home/Home.tsx
+++ Cake/src/Components/Home/Home.tsx
@@ -1,6 +1,5 @@
 import style from "./Home.module.css";
 import basket from "../../assets/Basket.svg";
-import { useState } from "react";
 import hamburger from "../../assets/menu.svg";
 import phone from "../../assets/phone.svg";
 import image9 from "../../assets/image 9.png";
@@ -8,15 +7,21 @@ import image8 from "../../assets/image 8.png"
 import image7 from "../../assets/image 7.png"
 import image6 from "../../assets/image 6.png"
 import image5 from "../../assets/image 5.png"
-
+import text from "../../assets/text new.png"
+import textMobile from "../../assets/textmobile.png"
 import button from "../../assets/Button.svg";
+import buttonMobile from "../../assets/Includebuttonmobile.png";
+import cake1 from "../../assets/cake1.png"
 
 function Home() {
-  const [responsive, setResponsive] = useState(true);
 
-  const toggleMenu = () => {
-    setResponsive(!responsive);
-  };
+  // const responsive = {
+  //   mobile: {
+  //     breakpoint: { max: 480, min: 0 },
+  //     items: 1,
+  //     slidesToSlide: 1,
+  //   }
+  // };
 
   return (
     <div className={style.container}>
@@ -31,7 +36,7 @@ function Home() {
               <img src={basket} alt="basket" />
             </li>
             <li>
-              <img src={hamburger} alt="menu" onClick={toggleMenu} />
+              <img src={hamburger} alt="menu"  />
             </li>
           </ul>
         </nav>
@@ -49,11 +54,13 @@ function Home() {
       </header>
       <div className={style.make}>
         <div className={style.make__container}>
-          <div className={style.cake_text}>Make Your Own Cake</div>
-          <div className="bottom_make">
-            <div>
-              <img src={button} alt="" />
-            </div>
+          <img className={style.text} src={text} alt="" />
+          <img className={style.textMobile} src={textMobile} alt="text" />
+          <div className={style.bottom_make}>
+              <img src={button} alt="button" />
+          </div>
+          <div className={style.bottom_make_mobile}>
+              <img src={buttonMobile} alt="button" />
           </div>
         </div>
       </div>
@@ -67,6 +74,9 @@ function Home() {
             <div className={style.cake_name}>Vanil Cake</div>
             <div className={style.cake_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores!</div>
             <div className={style.price}><span className="">$ 15</span><span>11.35</span></div>
+            <div className={style.cover_button}>
+            <button className={style.hovered_button}>Add to card</button>
+            </div>
           </div>
           <div className={style.box}>
             <div className={style.box_image}>
@@ -75,6 +85,9 @@ function Home() {
             <div className={style.cake_name}>Passion Dragon</div>
             <div className={style.cake_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores!</div>
             <div className={style.price}><span className="">$ 15</span><span>11.35</span></div>
+            <div className={style.cover_button}>
+            <button className={style.hovered_button}>Add to card</button>
+            </div>
           </div>
           <div className={style.box}>
             <div className={style.box_image}>
@@ -83,6 +96,9 @@ function Home() {
             <div className={style.cake_name}>Fondon</div>
             <div className={style.cake_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores!</div>
             <div className={style.price}><span className="">$ 15</span><span>11.35</span></div>
+            <div className={style.cover_button}>
+            <button className={style.hovered_button}>Add to card</button>
+            </div>
           </div>
           <div className={style.box}>
             <div className={style.box_image}>
@@ -91,6 +107,9 @@ function Home() {
             <div className={style.cake_name}>Chocolate Wow</div>
             <div className={style.cake_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores!</div>
             <div className={style.price}><span className="">$ 15</span><span>11.35</span></div>
+            <div className={style.cover_button}>
+            <button className={style.hovered_button}>Add to card</button>
+            </div>
           </div>
           <div className={style.box}>
             <div className={style.box_image}>
@@ -99,6 +118,9 @@ function Home() {
             <div className={style.cake_name}>Vanil Cake</div>
             <div className={style.cake_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores!</div>
             <div className={style.price}><span className="">$ 15</span><span>11.35</span></div>
+            <div className={style.cover_button}>
+            <button className={style.hovered_button}>Add to card</button>
+            </div>
           </div>
           <div className={style.box}>
             <div className={style.box_image}>
@@ -107,6 +129,9 @@ function Home() {
             <div className={style.cake_name}>Vanil Cake</div>
             <div className={style.cake_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores!</div>
             <div className={style.price}><span className="">$ 15</span><span>11.35</span></div>
+            <div className={style.cover_button}>
+            <button className={style.hovered_button}>Add to card</button>
+            </div>
           </div>
           <div className={style.box}>
             <div className={style.box_image}>
@@ -115,6 +140,9 @@ function Home() {
             <div className={style.cake_name}>Vanil Cake</div>
             <div className={style.cake_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores!</div>
             <div className={style.price}><span className="">$ 15</span><span>11.35</span></div>
+            <div className={style.cover_button}>
+            <button className={style.hovered_button}>Add to card</button>
+            </div>
           </div>
           <div className={style.box}>
             <div className={style.box_image}>
@@ -123,6 +151,39 @@ function Home() {
             <div className={style.cake_name}>Vanil Cake</div>
             <div className={style.cake_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores!</div>
             <div className={style.price}><span className="">$ 15</span><span>11.35</span></div>
+            <div className={style.cover_button}>
+            <button className={style.hovered_button}>Add to card</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={style.newProducts}>
+        <div className={style.left}>
+          <div className={style.new}>New Products</div>
+          <div className={style.awesome}>We Provide New Awesome Cakes</div>
+          <div className={style.last_text}>A cake is a sweetened baked food, usually made with flour, eggs, sugar, and a leavening agent. Shortening may or may not be used. </div>
+        </div>
+        <div className={style.right}>
+          <div className={style.cake}>
+            <img src={cake1} alt="" />
+            <div className={style.price_name}>
+              <div className={style.name_cake}>Fruitlane</div>
+              <div className="price">11$</div>
+            </div>
+          </div>
+          <div className={style.cake}>
+            <img src={cake1} alt="" />
+            <div className={style.price_name}>
+              <div className={style.name_cake}>Fruitlane</div>
+              <div className="price">11$</div>
+            </div>
+          </div>
+          <div className={style.cake}>
+            <img src={cake1} alt="" />
+            <div className={style.price_name}>
+              <div className={style.name_cake}>Fruitlane</div>
+              <div className="price">11$</div>
+            </div>
           </div>
         </div>
       </div>
